5cabd28ffd8855bb00212735d9c261add7b987f7
PussyHunteRR <74722607+PussyHunteRR@users.noreply.github.com>
2021-03-03 19:17:22 +0300

    Test
    Test

---
+++ EFDataApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFDataApp.Models; 
+
+namespace EFDataApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Books.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Book book)
+        {
+            db.Books.Add(book);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EFDataApp/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFDataApp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   
+        }
+    }
+}
